3eb7bd3c72c52a9fd6643de0dbcb00bf28752eb9
achilleleal <achilleleal@gmail.com>
2020-08-17 15:48:22 -0400

    Updates

---
+++ precache-manifest.6b0c07f80e2b862816e06fc71272e0fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5efec25571b2a77b2b94e3e7e551445f",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "e60102e8f21dfbdb1baf",
+    "url": "/Pokedex/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3fa02966c23b90e079d8",
+    "url": "/Pokedex/static/css/main.3c8f0fef.chunk.css"
+  },
+  {
+    "revision": "e60102e8f21dfbdb1baf",
+    "url": "/Pokedex/static/js/2.91d206ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pokedex/static/js/2.91d206ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fa02966c23b90e079d8",
+    "url": "/Pokedex/static/js/main.e71e72b6.chunk.js"
+  },
+  {
+    "revision": "4cd505cec87fc8844e77",
+    "url": "/Pokedex/static/js/runtime-main.fb64d595.js"
+  }
+]);
